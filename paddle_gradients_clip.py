5cd7ffef8276cdf1bc3ffee345fc2ca4b2b5ae1f
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-03-01 06:25:15 +0000

    gradient

--- paddle_gradients_clip.py
+++ paddle_gradients_clip.py
@@ -22,4 +22,8 @@ linear = paddle.nn.Linear(10, 10, weight_attr=paddle.ParamAttr(need_clip=False))
 
 
 # clip gradient by global norm
+linear = paddle.nn.Linear(10, 10)
+clip = paddle.nn.ClipGradByGloabalNorm(clip_norm=1.0)
+sdg = paddle.optimizer.SGD(learning_rate=0.1, parameters=linear.parameters(), grad_clip=clip)
+
 
